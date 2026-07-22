0e1eccb7805046d845b1547daf6f61ba38e06cd1
otakosan <75729130+otakosan@users.noreply.github.com>
2026-07-22 12:16:14 +0100

    Deploy AI business ideas Kindle ebook

---
+++ _next/static/chunks/app/ebooks/[slug]/page-8e55500a4a160ede.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[97,62,125,923,906],{9050:function(n,e,u){Promise.resolve().then(u.t.bind(u,9498,23))}},function(n){n.O(0,[498,649,945,744],function(){return n(n.s=9050)}),_N_E=n.O()}]);
